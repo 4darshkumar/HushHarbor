addba518c04b63321442cb5ebf560194bf5fe308
4darshkumar <adarsh.kumar181199@gmail.com>
2024-02-07 23:29:23 +0530

    added some components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Home.css';
+import ProfileSide from "../components/ProfileSide";
+
+const Home = () =>{
+    return (<div>
+    <div className="profileSide">sgspd
+    </div>
+    <div className="postSide">Posts</div>
+    <div className="RightSide">Trending</div>
+
+    </div>
+
+    )
+
+}
+export default Home;
